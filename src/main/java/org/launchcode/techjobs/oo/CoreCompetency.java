7957777f5c6730d1e3faf828389526a2e509c5ae
Victoria <veerod0804@yahoo.com>
2022-09-26 14:16:34 -0500

    Finished Task 1 in Assignment 2

--- src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
+++ src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
@@ -41,4 +41,17 @@ public class CoreCompetency {
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
     //  ONLY a getter for the 'id' field.
 
+    public String getValue() {
+        return value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+
+
+    }
 }
